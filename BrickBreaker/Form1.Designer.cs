82979bbda461a1a39dffd7d7d438e4061d0660a9
Parkrail211 <78805057+Parkrail211@users.noreply.github.com>
2021-12-06 15:17:52 -0500

    files lmao

---
+++ BrickBreaker/Form1.Designer.cs
@@ -0,0 +1,53 @@
+﻿namespace BrickBreaker
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(96F, 96F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Dpi;
+            this.BackColor = System.Drawing.Color.LightBlue;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.ClientSize = new System.Drawing.Size(854, 542);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
+
---
+++ BrickBreaker/PowerUp.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+
+namespace BrickBreaker
+{
+    class PowerUp
+    {
+       
+        public string type;
+        public float x, y;
+        public Brush brush;
+
+        public PowerUp(int rand, float _x, float _y)
+        {
+
+            string[] powerNames = new string[] { "scatterShot", "wumbo", "krabbyPatty" };
+            string power = powerNames[rand];
+            #region colourSelection
+            if (power == "scatterShot")
+            {
+                 brush = new SolidBrush(Color.Blue);
+            }
+            else if (power == "wumbo")
+            {
+                brush = new SolidBrush(Color.Gold);
+            }
+            else
+            {
+                brush = new SolidBrush(Color.White);
+            }
+            #endregion
+
+            type = power;
+            x = _x;
+            y = _y;
+        }
+
+       public void Move()
+        {
+            y += 5;
+        }
+
+        public bool PaddleCollision(Paddle p)
+        {
+            
+            Rectangle powerRec = new Rectangle(Convert.ToInt32(x), Convert.ToInt32(y), 20, 20);
+            Rectangle paddleRec = new Rectangle(p.x, p.y, p.width, p.height);
+
+            if (powerRec.IntersectsWith(paddleRec))
+            {
+                
+                return true;
+            }
+
+            return false;
+        }
+    }
+}
