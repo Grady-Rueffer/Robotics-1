9a98bd1762d849695c04d08426dec9e3143018e3
Parkrail211 <parkrail211@ed.amdsb.ca>
2021-12-06 15:27:21 -0500

    oops

---
+++ v5code-project-roboTeamCode/src/main.cpp
@@ -0,0 +1,136 @@
+/*----------------------------------------------------------------------------*/
+/*                                                                            */
+/*    Module:       main.cpp                                                  */
+/*    Author:       C:\Users\Parker                                           */
+/*    Created:      Tue Nov 09 2021                                           */
+/*    Description:  V5 project                                                */
+/*                                                                            */
+/*----------------------------------------------------------------------------*/
+
+// ---- START VEXCODE CONFIGURED DEVICES ----
+// Robot Configuration:
+// [Name]               [Type]        [Port(s)]
+// Controller1          controller
+// RightMotor           motor         1
+// LeftMotor            motor         10
+// ---- END VEXCODE CONFIGURED DEVICES ----
+
+#include "vex.h"
+// :^)
+using namespace vex;
+
+bool bounceDetect(float lastSpeed) {
+  int decelBuffer = 50;
+  if (Inertial7.acceleration(xaxis) < lastSpeed - decelBuffer) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+void turn(bool rDir, int rotationDegrees) {
+  RightMotor.setVelocity(0, percent);
+  LeftMotor.setVelocity(0, percent);
+
+  if (rDir) {
+    double rotationTotal = Inertial7.rotation(degrees) + rotationDegrees;
+    while (Inertial7.rotation(degrees) < rotationTotal) {
+
+      RightMotor.spin(reverse);
+      LeftMotor.spin(forward);
+
+      RightMotor.setVelocity(50, percent);
+      LeftMotor.setVelocity(50, percent);
+    }
+  } else {
+    double rotationTotal = Inertial7.rotation(degrees) - rotationDegrees;
+    while (Inertial7.rotation(degrees) > rotationTotal) {
+
+      RightMotor.spin(forward);
+      LeftMotor.spin(reverse);
+
+      RightMotor.setVelocity(50, percent);
+      LeftMotor.setVelocity(50, percent);
+    }
+  }
+  RightMotor.setVelocity(0, percent);
+  LeftMotor.setVelocity(0, percent);
+}
+
+void autoMode(bool rSide) {
+  // uncurl
+
+  if (rSide) {
+    // move forward lil bit
+    // rotate 45ish degrees right
+    // move forward until hit goal (method TBD)
+
+    int lastSpeed = Inertial7.acceleration(xaxis);
+
+    while (!bounceDetect(lastSpeed)) {
+      RightMotor.setVelocity(50, percent);
+      LeftMotor.setVelocity(50, percent);
+
+      lastSpeed = Inertial7.acceleration(xaxis);
+    }
+
+    // move back small amount
+    // turn so arm is over pole
+    // dump the rings
+
+    // turn back
+    // move forwards
+    // pick up goal
+    // move back until past line
+    // rotate goal
+    // put down
+
+    // move to other goal
+    // pickup
+    // bring back
+
+  } else {
+    // angle bot
+    // dump rings
+    // unangle bot
+    // pick up goal
+    // move onto goal
+  }
+}
+
+bool rc = true;
+int deadBand = 5;
+int rightSpeed;
+int leftSpeed;
+
+int main() {
+  // Initializing Robot Configuration. DO NOT REMOVE!
+  vexcodeInit();
+
+  while (rc) {
+    // how fast it move
+    rightSpeed = Controller1.Axis2.position(percent);
+    leftSpeed = Controller1.Axis3.position(percent);
+    // antistick-drift
+    if (abs(rightSpeed) < deadBand) {
+      RightMotor.setVelocity(0, percent);
+
+    } else {
+      RightMotor.setVelocity(rightSpeed, percent);
+    }
+
+    if (abs(leftSpeed) < deadBand) {
+      LeftMotor.setVelocity(0, percent);
+
+    } else {
+      LeftMotor.setVelocity(leftSpeed, percent);
+    }
+    RightMotor.spin(forward);
+    LeftMotor.spin(forward);
+  }
+  if (Controller1.ButtonRight.pressing()) {
+    autoMode(1);
+  } else if (Controller1.ButtonLeft.pressing()) {
+    autoMode(0);
+  }
+}
---
+++ v5code-project-roboTeamCode/src/robot-config.cpp
@@ -0,0 +1,27 @@
+#include "vex.h"
+
+using namespace vex;
+using signature = vision::signature;
+using code = vision::code;
+
+// A global instance of brain used for printing to the V5 Brain screen
+brain  Brain;
+
+// VEXcode device constructors
+controller Controller1 = controller(primary);
+motor RightMotor = motor(PORT1, ratio36_1, true);
+motor LeftMotor = motor(PORT10, ratio36_1, false);
+inertial Inertial7 = inertial(PORT7);
+
+// VEXcode generated functions
+// define variable for remote controller enable/disable
+bool RemoteControlCodeEnabled = true;
+
+/**
+ * Used to initialize code/tasks/devices added using tools in VEXcode Pro.
+ * 
+ * This should be called at the start of your int main function.
+ */
+void vexcodeInit( void ) {
+  // nothing to initialize
+}
